4a63eabaafe5ec6ef9bfc92d74e0b89df9d68fba
cudeey <od61831@ubt-uni.net>
2023-02-22 22:09:34 +0100

    shtimi i javascript per faqet e reja

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-"use strict";
-
 /**
  * navbar variables
  */
@@ -42,3 +40,22 @@ window.addEventListener("scroll", function () {
     ? goTopBtn.classList.add("active")
     : goTopBtn.classList.remove("active");
 });
+
+/* javascripti i faq */
+
+var faq = document.getElementsByClassName('faq-page');
+var i = 0;
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+        /* Toggle between hiding and showing the active panel */
+        var body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
+}
